e9ae746088286ebbd6c5c1f98d36b1737b782b9f
RANJAN KUMAR <92841369+ranjangupta4590@users.noreply.github.com>
2024-07-31 23:30:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
       },
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
   
 module.exports = nextConfig
