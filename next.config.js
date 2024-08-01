1c926083199c2eb851aac8023f657ae577b625bb
ranjan_4590 <ranjangupta35558@gmail.com>
2024-08-01 10:18:45 +0530

    config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
       },
-    eslint: {
-    ignoreDuringBuilds: true,
-  },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
 }
   
 module.exports = nextConfig
